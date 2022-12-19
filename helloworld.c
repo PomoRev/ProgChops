875f0aed5abed87ac574ae840ca7b8ddb6e06078
Frank Emanuel <emanuef@algonquincollege.com>
2022-12-19 15:18:28 -0500

    intro code

---
+++ helloworld.c
@@ -0,0 +1,18 @@
+/* helloworld.c
+ * 
+ * Prof. Frank Emanuel, Ph.D. 
+ * Programming Chops Video Series
+ * 2022-12-19
+ */
+
+#include <stdio.h>
+
+int main(){
+
+    printf( "Hello World!\n" );
+
+    return 0;
+    
+}
+
+
