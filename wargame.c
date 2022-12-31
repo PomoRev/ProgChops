1b41ab3a1004fc5ca40ae8ddd136ae01f9e4c3e0
Frank Emanuel <emanuef@algonquincollege.com>
2022-12-31 14:53:36 -0500

    added initial comments example

---
+++ wargame.c
@@ -0,0 +1,41 @@
+/* wargame.c
+ * 
+ * Prof. Frank Emanuel, Ph.D. 
+ * Programming Chops Video Series
+ * 2022-12-31
+ */
+
+#include <stdio.h>
+
+int main(){
+
+/* SETUP GAME */ 
+
+    /* Create a deck of cards */ 
+
+    /* Shuffle the deck of cards  */
+
+    /* Deal half of the deck of cards to each player */ 
+
+/* RUN GAME */ 
+
+    /* Until one player has all of the cards */ 
+
+        /* Each player reveals their top card  */
+
+        /* Until the cards the same value?  */
+
+            /* Each player discards three cards (WAR)  */
+
+            /* Each player reveals their top card */ 
+
+        /* Place all discarded and revealed cards at bottom of winner's deck */ 
+
+    /* Declare the winner of the game */ 
+
+    return 0;
+}
+
+
+
+
