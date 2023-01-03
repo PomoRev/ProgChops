ebd8d2bfa4823726618c8f6bf4b95d1003ddef4c
Frank Emanuel <emanuef@algonquincollege.com>
2023-01-03 12:18:49 -0500

    after video

--- simple.js
+++ simple.js
@@ -5,3 +5,29 @@
     Frank Emanuel (c) 2023
 */
 
+/* 
+    This is the initial code example that combines the values always as if they 
+    text. 
+
+function combine() {
+
+    result.innerText = inputA.value + inputB.value;
+
+} 
+*/
+
+function combine() {
+
+    if ( isNaN(inputA.value) || isNaN(inputB.value) ){
+
+        result.innerText = inputA.value + " & " + inputB.value;
+
+    } else {
+
+        result.innerText = parseInt(inputA.value) + parseInt(inputB.value);
+
+    }
+
+    return;
+
+}
