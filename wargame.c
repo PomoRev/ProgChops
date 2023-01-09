afb7414cb3c393ad82f3025944cd040f4ae0f0dd
Frank Emanuel <emanuef@algonquincollege.com>
2023-01-08 23:47:05 -0500

    after video

--- wargame.c
+++ wargame.c
@@ -7,11 +7,60 @@
 
 #include <stdio.h>
 
+struct card {
+    int value;
+    int suit;
+} deckOfCards[52];
+
+void setUpInitialDeck(){
+
+/*  adds a standard 52 card deck deckOfCards using 
+    4 suits and 13 values 
+*/
+
+    int i, j, deckOffset = 0;
+
+    for( i = 1; i < 5; i++ ){
+
+        for ( j = 1; j < 14; j++){
+
+            deckOfCards[deckOffset].suit = i;
+            deckOfCards[deckOffset].value = j;
+            deckOffset++;
+        }
+    }
+
+    return;
+}
+
+void testDeck ( struct card deck[] ){
+
+/*  print out each of the cards in the deck 
+ */
+
+    int offsetInDeck;
+
+    for ( offsetInDeck = 0; offsetInDeck < 52; offsetInDeck++ ){
+
+        printf ( "[ %d, %d] ", 
+            deck[offsetInDeck].suit, 
+            deck[offsetInDeck].value);
+    }
+
+    printf( "\n" );
+
+    return;
+}
+
 int main(){
 
 /* SETUP GAME */ 
 
     /* Create a deck of cards */ 
+
+    setUpInitialDeck();
+
+    testDeck ( deckOfCards );
 
     /* Shuffle the deck of cards  */
 
